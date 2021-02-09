804173c188cb7174fa54fe18847f7efaaf3632a2
Guilherme de jesus <gui-rock02@hotmail.com>
2021-02-09 08:30:22 -0300

    Segundo Commit

---
+++ main.js
@@ -0,0 +1,4 @@
+function redirecionar(){
+    //window.open("https://www.sexshopsp.com.br/bomba-peniana?gclid=Cj0KCQiA34OBBhCcARIsAG32uvPlDzgMqa1DsrXXZV8o9pVe_f9jMlx8Kf7OHQkAQ1p-Uia4T3EydoUaAkZSEALw_wcB");
+    window.open("https://www.instagram.com/change-account")
+}
